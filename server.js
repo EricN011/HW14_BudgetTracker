53b212d7852a2fe23e58fcdbfa0b816659bd7951
Eric Noll <eanoll88@gmail.com>
2020-02-10 18:27:08 -0600

    routes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes here
+app.use(require("./routes/apiRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
